62b4184266ab02ab90fc0791214c671bfcc87c2a
Ravil Khabibullin <ravil.khabibullin94@gmail.com>
2021-01-21 15:18:31 +0300

    get user info by cookie and export post info as json

--- insta_crawler/cli.py
+++ insta_crawler/cli.py
@@ -1,14 +1,19 @@
+import json
+import os
+from collections import OrderedDict
 from pprint import pprint
 from time import sleep
 
 import click
-from prettytable import PrettyTable, ALL
+
 from insta import InstaCrawler
-from utils import download_file, download_all
+from utils import (download_all, download_file,
+                   export_as_json, print_user_info_table,
+                   print_single_post_info_table)
 
 
 @click.group()
-def cli():
+def get_insta():
     """
     hello there.
 
@@ -16,11 +21,20 @@ def cli():
     click.echo("Starting")
 
 
-@cli.command()
-@click.option("-C", "--cookie",
+@get_insta.command()
+@click.option("-C", "--cookie", required=True,
               help="You can paste your cookie")
+def cookie_user(cookie: str):
+    inst = InstaCrawler(cookie=cookie)
+    cookie_user = inst.get_cookie_user()
+
+    print_user_info_table(user_info=cookie_user)
+
+
+@get_insta.command()
 @click.option("-u", "--username",
               required=True, help="Username Required")
+@click.option("-C", "--cookie", help="You can paste your cookie")
 def user_info(cookie: str, username: str):
     """
     This command provides you some information about the account.
@@ -35,35 +49,12 @@ def user_info(cookie: str, username: str):
     USER_URL = "https://www.instagram.com/{username}/"
     user_info = inst.get_user_info(url=USER_URL.format(username=username))
 
-    table = PrettyTable(padding_width=5)
-    table.field_names = ["FIELD NAME", "INFO"]
-
-    tds = {
-        "Instagram URL": USER_URL.format(username=username),
-        "Username": user_info["username"],
-        "Bio": fr'{user_info["bio"]}',
-        "Full Name": user_info["full_name"].replace("\t", "-"),
-        "Business Category": user_info["business_category_name"],
-        "Category": user_info["category_name"],
-        "Followers": user_info["edge_followed_by"],
-        "Follows": user_info["edge_follow"],
-        "Posts": user_info["posts_count"],
-        "Reels": user_info["highlight_reel_count"],
-        "IGTV": user_info["igtv_count"],
-        "Is Private": user_info["is_private"],
-        "ID": user_info["id"],
-        "Ext URL": user_info["external_url"],
-    }
-
-    for field, info in tds.items():
-        table.add_row([field, info])
-
-    print(table.get_string(title=f"Info about {username}`s instagram account"))
+    print_user_info_table(user_info=user_info)
 
 
-@cli.command()
-@click.option("-C", "--cookie", help="You can paste your cookie")
+@get_insta.command()
 @click.option("-u", "--url", required=True, help="URL Required")
+@click.option("-C", "--cookie", help="You can paste your cookie")
 def single_post(cookie: str, url: str):
     """
     This command provides you information about the post by url.
@@ -72,24 +63,7 @@ def single_post(cookie: str, url: str):
     inst = InstaCrawler(cookie=cookie)
     links = inst.get_single_post(url=url)
 
-    table = PrettyTable(padding_width=3)
-    table.field_names = ["FIELD NAME", "POST INFO"]
-    table._max_width = {"POST INFO": 100}
-
-    for field, info in links.items():
-        post_links = []
-
-        if field == "post_content":
-            for i in range(len(links["post_content"])):
-                post_links.append(
-                    [f"link {i + 1}", f'{links["post_content"][i]}\n'])
-            table.add_rows(post_links)
-            continue
-
-        table.add_row([field, f"{info}\n"])
-
-    print(table.get_string(
-        title=f"{links['owner_username']}`s instagram post"))
+    print_single_post_info_table(post_info=links)
 
     if click.confirm("\nDownload the post`s content?", abort=True):
         with click.progressbar(length=len(links["post_content"]),
@@ -108,16 +82,15 @@ def single_post(cookie: str, url: str):
     click.echo("\nDone")
 
 
-@cli.command()
-@click.option("-C", "--cookie",
-              help="You can paste your cookie")
-@click.option("-u", "--username",
-              required=True, help="Username Required")
+@get_insta.command()
 @click.option("-ct", "--content-type",
               type=click.Choice(
                   ["posts", "stories", "highlights", "igtv", "all"],
                   case_sensitive=False))
-def ctgry(cookie: str, username: str, content_type: str):
+@click.option("-u", "--username",
+              required=True, help="Username Required")
+@click.option("-C", "--cookie", help="You can paste your cookie")
+def category(cookie: str, username: str, content_type: str):
     """
     all posts func help message
     """
@@ -143,18 +116,21 @@ def ctgry(cookie: str, username: str, content_type: str):
         }
 
     click.echo("Данные страницы получены.")
+    if click.confirm("Сохранить данные в JSON?"):
+        export_as_json(data=data, username=username)
 
-    for ct, value in data.items():
-        if value:
-            click.echo(f'Начинаю скачивать категорию {ct}...')
-            download_all(posts=value,
-                         content_type=ct,
-                         username=username)
-        else:
-            continue
+    if click.confirm("Загрузить содержимое?", abort=True):
+        for ct, value in data.items():
+            if value:
+                click.echo(f'Начинаю скачивать категорию {ct}...')
+                download_all(posts=value,
+                             content_type=ct,
+                             username=username)
+            else:
+                continue
 
-    click.echo("\nГотово!")
+    click.echo("Готово!")
 
 
 if __name__ == "__main__":
-    cli()
+    get_insta()
