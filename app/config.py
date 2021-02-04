df9fdac9555bd920ca7df2dd227ec13a24befd5e
Ravil Khabibullin <ravil.khabibullin94@gmail.com>
2021-02-04 14:12:50 +0300

    config file with csv headers rows

---
+++ app/config.py
@@ -0,0 +1,12 @@
+category_headers_row = ["shortcode", "post_link", "owner_username",
+                        "owner_link", "likes", "comments",
+                        "description", "title", "posted_at",
+                        "post_content", "post_content_len"]
+
+followers_headers_row = ["id", "username", "full_name", "user_url",
+                         "bio", "posts_count", "igtv_count",
+                         "highlight_reel_count", "edge_followed_by",
+                         "edge_follow", "is_private", "followed_by_viewer",
+                         "is_business_account", "business_category_name",
+                         "category_name", "external_url", "profile_pic_hd",
+                         "last_12_posts_shortcodes"]
