0822f9b24e87d93cf472b916bdc0c3d5d409bc4f
Ravil Khabibullin <ravil.khabibullin94@gmail.com>
2021-02-09 15:15:44 +0300

    moved constants from init

--- app/insta_crawler/insta.py
+++ app/insta_crawler/insta.py
@@ -23,10 +23,13 @@ class InstaCrawler:
     GRAPHQL_QUERY: str = "graphql/query/"
     STORIES_QUERY: str = "api/v1/feed/reels_media/"
 
-    all_posts_query_hash: str
-    user_reels_query_hash: str
-    followers_query_hash: str
-    followed_by_user_query_hash: str
+    all_posts_query_hash: str = "003056d32c2554def87228bc3fd9668a"
+    user_reels_query_hash: str = "d4d88dc1500312af6f937f7b804c68c3"
+    followers_query_hash: str = "c76146de99bb02f6415203be841dd25a"
+    followed_by_user_query_hash: str = "d04b0a864b4b54837c0d870b0e77e076"
+    user_igtvs_query_hash: str = "bc78b344a68ed16dd5d7f264681c4c76"
+    cookie_user_timeline_hash: str = "b1245d9d251dff47d91080fbdd6b274a"
+    x_ig_app_id: str = "936619743392459"
 
     user_info: Union[Dict, None]
 
@@ -34,19 +37,11 @@ class InstaCrawler:
     # example: "ig_did=XXXXXXXX-YYYY-CCCC-AAAA-ZZZZZZZZZZZZ; sessionid=1111111111111111111111111;"
     cookie: str
 
-    def __init__(self, cookie):
+    def __init__(self, cookie: str):
         if not cookie:
             raise NoCookieError
         else:
             self.cookie = cookie
-
-        self.all_posts_query_hash = "003056d32c2554def87228bc3fd9668a"
-        self.user_reels_query_hash = "d4d88dc1500312af6f937f7b804c68c3"
-        self.user_igtvs_query_hash = "bc78b344a68ed16dd5d7f264681c4c76"
-        self.cookie_user_timeline_hash = "b1245d9d251dff47d91080fbdd6b274a"
-        self.followers_query_hash = "c76146de99bb02f6415203be841dd25a"
-        self.followed_by_user_query_hash = "d04b0a864b4b54837c0d870b0e77e076"
-        self.x_ig_app_id = "936619743392459"
 
         self.user_info = None
 
