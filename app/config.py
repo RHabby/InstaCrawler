f6ced71c7d3f3355edd3cf8c2890fcde9cf7e497
Ravil Khabibullin <ravil.khabibullin94@gmail.com>
2021-09-30 17:16:57 +0300

    headers for csv writer

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 import os
 
+from app.insta_crawler.models import Highlight, IGTV, Post, Storie, User
 from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -8,15 +9,19 @@ load_dotenv(dotenv_path=os.path.join(basedir, "..", ".env"))
 login = os.environ.get("LOGIN2")
 password = os.environ.get("PASSWORD2")
 
-category_headers_row = ["shortcode", "post_link", "owner_username",
-                        "owner_link", "likes", "comments",
-                        "description", "title", "posted_at",
-                        "post_content", "post_content_len"]
+category_headers_row = {
+    "posts": list(Post.__fields__.keys()),
+    "stories": list(Storie.__fields__.keys()),
+    "highlights": list(Highlight.__fields__.keys()),
+    "igtv": list(IGTV.__fields__.keys()),
+}
 
-followers_headers_row = ["id", "username", "full_name", "user_url",
-                         "bio", "posts_count", "igtv_count",
-                         "highlight_reel_count", "edge_followed_by",
-                         "edge_follow", "is_private", "followed_by_viewer",
-                         "is_business_account", "business_category_name",
-                         "category_name", "external_url", "profile_pic_hd",
-                         "last_12_posts_shortcodes"]
+followers_headers_row = [
+    field
+    for field in User.__fields__.keys()
+    if field != "last_twelve_posts"
+]
+
+
+if __name__ == "__main__":
+    pass
